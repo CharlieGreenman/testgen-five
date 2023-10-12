345b4147cb2c6d4d5aed719ea99f3c42c319264b
Charlie Greenman <charlie@razroo.com>
2023-10-12 13:01:47 -0400

    Add code for auth-0-angular-16.2.0 add-auth-config.
    [Url: razroo.com/razroo/auth-0-angular-16.2.0/add-auth-config/add-auth-config]

---
+++ libs/common/common-services/src/lib/auth/auth-config.ts
@@ -0,0 +1,14 @@
+import { AuthConfig } from '@auth0/auth0-angular';
+
+export const authConfig: AuthConfig = {
+  // The app's clientId configured in Auth0
+  clientId: environment.clientId,
+  domain: environment.issuer,
+  useRefreshTokens: true,
+  cacheLocation: 'localstorage',
+  authorizationParams: {
+    scope: 'openid profile email offline_access read:roles',
+    redirect_uri: environment.hostUrl + '/',
+    audience: environment.apiIdentifier
+  },
+};
